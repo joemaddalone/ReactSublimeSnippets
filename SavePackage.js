fd92e492d2c1a9aef3666270971a9ce116d0d3a5
Joe Maddalone <joe@insert-title.com>
2014-03-16 11:35:25 -0500

    tweaked cursor position on rren

--- SavePackage.js
+++ SavePackage.js
@@ -2,4 +2,5 @@ var EasyZip = require('easy-zip').EasyZip;
 var zip = new EasyZip();
 zip.zipFolder('snippets',function(){
     zip.writeToFile('ReactSnippets.sublime-package');
+    console.log("Rendered Package")
 });
