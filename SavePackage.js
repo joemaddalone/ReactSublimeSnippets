4dfee215eb9cbd7a66cf26ed23b298a4d9a31382
Joe Maddalone <joe@insert-title.com>
2014-12-25 19:11:51 -0600

    Use compilesnippets package rather than local scripts

--- SavePackage.js
+++
@@ -1,6 +0,0 @@
-var EasyZip = require('easy-zip').EasyZip;
-var zip = new EasyZip();
-zip.zipFolder('snippets',function(){
-    zip.writeToFile('ReactSnippets.sublime-package');
-    console.log("Rendered Package")
-});
